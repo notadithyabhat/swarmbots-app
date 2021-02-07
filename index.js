9c8fac65f9380782ba65e4b907e6bdacdb08478c
Adithya Bhat <adithyabhat7@gmail.com>
2021-02-07 08:51:46 +0530

    Canvas completed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('canvas')
 const c = canvas.getContext('2d') //context
 
-canvas.width = innerWidth 
-canvas.height = innerHeight
+canvas.width = innerWidth/1.5
+canvas.height = innerHeight/1.5
 
 const columns = 12, rows = 9;
 
@@ -10,32 +10,37 @@ const tileWidth  = Math.round(canvas.width / columns),
  tileHeight = Math.round(canvas.height / rows);
 
 
+function getFont(ratio) {
+    var size = canvas.width * ratio;
+    return (size|0) + 'px Arial';
+}
+
 
 class Destination {
-	constructor(id,x,y,tileWidth,tileHeight) {
+	constructor(id,x,y,Width,Height) {
 		this.x = x
 		this.y = y
 		this.color = "#398AD7"
 		this.id = id
-		this.tileWidth=tileWidth
-		this.tileHeight=tileHeight
+		this.Width=Width
+		this.Height=Height
 	}
 
 	draw() {
 		c.beginPath()
 		c.save()
-		c.lineWidth = this.tileWidth/20;
+		c.lineWidth = tileWidth/20;
 		c.strokeStyle = this.color
 		c.shadowColor = this.color
 		c.shadowBlur = 30
-		c.strokeRect(this.x+this.tileWidth/40,this.y+this.tileWidth/40,this.tileWidth-this.tileWidth/20,this.tileHeight-this.tileWidth/20)
+		c.strokeRect(this.x+tileWidth/40,this.y+tileWidth/40,this.Width-tileWidth/20,this.Height-tileWidth/20)
 		c.font = "20px Arial"
 		c.fillStyle = this.color
 		c.textAlign = "center"
-		c.fillText("AGV"+this.id, this.x+this.tileWidth/2, this.y+this.tileHeight/2)
+		c.fillText("AGV"+this.id, this.x+this.Width/2, this.y+this.Height/2)
 		c.restore()
 	}
-	static draw(x,y,color,tileWidth,tileHeight)
+	static draw(x,y,color,Width,Height)
 	{
 		if(x!=null&&y!=null){
 			c.beginPath()
@@ -44,11 +49,11 @@ class Destination {
 			c.strokeStyle = color
 			c.shadowColor = color
 			c.shadowBlur = 30
-			c.strokeRect(x+tileWidth/40,y+tileWidth/40,tileWidth-tileWidth/20,tileHeight-tileWidth/20)
+			c.strokeRect(x+tileWidth/40,y+tileWidth/40,Width-tileWidth/20,Height-tileWidth/20)
 			c.shadowBlur = 0
 			c.fillStyle = color
 			c.textAlign = "center"
-			c.fillText("("+x/tileWidth+","+y/tileHeight+")",x+tileWidth/2, y+tileHeight/2)
+			c.fillText("("+x/tileWidth+","+y/tileHeight+")",x+Width/2, y+Height/2)
 			c.restore()
 		}
 		
@@ -76,13 +81,6 @@ class Agv {
 		c.beginPath()
 		c.save()
 		if(this.hover||this.selected){
-			info.innerHTML = "AGV "+this.id+"<br>X:"+Math.round(this.x/tileWidth)+"<br>Y:"+Math.round(this.y/tileHeight);
-			info.style.left = this.x+tileWidth+'px';
-			info.style.top = this.y+tileHeight+'px';
-			if(this.selected)
-			{
-				this.color = "#73D393"
-			}
 			c.shadowColor = this.color
 			c.shadowBlur = 15
 		}
@@ -93,11 +91,11 @@ class Agv {
 		c.strokeRect(this.x+(tileWidth/10), this.y+(tileWidth/10), tileWidth-tileWidth/5, tileHeight-tileWidth/5);
 		c.fillRect(this.x+(tileWidth/10), this.y+(tileWidth/10), tileWidth-tileWidth/5, tileHeight-tileWidth/5);
 		c.restore()
-		c.font = "20px Arial"
+		c.font = getFont(0.02)
 		c.fillStyle = "white"
 		c.textAlign = "center"
 		c.fillText("AGV"+this.id, this.x+tileWidth/2, this.y+tileHeight/2)
-		c.font = "15px Arial"
+		c.font = getFont(0.012)
 		c.fillText(this.state,this.x+tileWidth/2, this.y+tileHeight/2+15)
 	}
 
@@ -139,13 +137,13 @@ class Agv {
 class SuperAgv {
 	constructor(id,agv1,agv2,state='AVAILABLE')
 	{
-		this.agv1=agv1
-		this.agv2=agv2
+		this.agv1=((agv1.x<agv2.x)||(agv1.y<agv2.y))?agv1:agv2
+		this.agv2=(this.agv1==agv1)?agv2:agv1
 		this.id=id
-		this.x=Math.min(agv1.x,agv2.x)
-		this.y=Math.min(agv1.y,agv2.y)
-		this.x2=Math.max(agv1.x,agv2.x)
-		this.y2=Math.max(agv1.y,agv2.y)
+		this.x=this.agv1.x
+		this.y=this.agv1.y
+		this.x2=this.agv2.x
+		this.y2=this.agv2.y
 		this.color="#50C878"
 		this.state=state
 		this.angle=0
@@ -172,13 +170,6 @@ class SuperAgv {
 	c.beginPath()
 		c.save()
 		if(this.hover||this.selected){
-			info.innerHTML = "AGV "+this.id+"<br>X:"+Math.round(this.x/tileWidth)+"<br>Y:"+Math.round(this.y/tileHeight);
-			info.style.left = this.x+tileWidth+'px';
-			info.style.top = this.y+tileHeight+'px';
-			if(this.selected)
-			{
-				this.color = "#73D393"
-			}
 			c.shadowColor = this.color
 			c.shadowBlur = 15
 		}
@@ -189,11 +180,11 @@ class SuperAgv {
 		c.strokeRect(this.x+(this.tileWidth/10), this.y+(this.tileWidth/10), this.tileWidth-this.tileWidth/5, this.tileHeight-this.tileWidth/5);
 		c.fillRect(this.x+(this.tileWidth/10), this.y+(this.tileWidth/10), this.tileWidth-this.tileWidth/5, this.tileHeight-this.tileWidth/5);
 		c.restore()
-		c.font = "20px Arial"
+		c.font = getFont(0.02)
 		c.fillStyle = "white"
 		c.textAlign = "center"
 		c.fillText("AGV"+this.agv1.id+"+"+this.agv2.id, this.x+this.tileWidth/2, this.y+this.tileHeight/2)
-		c.font = "15px Arial"
+		c.font = getFont(0.012)
 		c.fillText(this.state,this.x+this.tileWidth/2, this.y+this.tileHeight/2+15)
 	}
 
@@ -279,7 +270,7 @@ function checkNeighbors(mode,agv) {
 	var agv1=null
 	agvs.some(function(agv2) {
 		if((Math.abs(agv2.x-agv.x)==tileWidth&&(agv2.y==agv.y))||(Math.abs(agv2.y-agv.y)==tileHeight&&(agv2.x==agv.x))) {
-			if(!agv2.super&&!agv.super){
+			if(!agv2.super&&!agv.super&&agv.state!='UNAVAILABLE'&&agv2.state!='UNAVAILABLE'){
 				agv1=agv2
 				flag=true
 			}
@@ -301,8 +292,8 @@ function removeAgv(agvs, agv){
 function destroyAgv(agv)
 {
 	if(agv.super){
-		var agv1 = new Agv(agv.agv1.id,agv.x/tileWidth,agv.y/tileHeight)
-		var agv2 = new Agv(agv.agv2.id,agv.x2/tileWidth,agv.y2/tileHeight)
+		var agv1 = new Agv(agv.agv1.id,agv.x/tileWidth,agv.y/tileHeight,agv.state)
+		var agv2 = new Agv(agv.agv2.id,agv.x2/tileWidth,agv.y2/tileHeight,agv.state)
 		return [agv1,agv2]
 
 	}
@@ -344,31 +335,14 @@ addEventListener('click', (event) =>
 		agvs.forEach((agv) => {
 			if((agv.x==x&&agv.y==y)&&(agv.state=='AVAILABLE')){
 					agv.selected = agv.selected? false : true
-					selectedAgv=agv
 			}
 			else {
 				if(agv.selected){
 					var flag=true;
-					agvs.forEach((agv2) => {
-						if(agv2.x==x&&agv2.y==y){
-							flag=false
-							agv.selected=false
-							selectedAgv=null
-						}
-						if(agv2.super&&(agv!=agv2))
-						{
-							if(agv2.x2==x&&agv2.y2==y){
-								flag=false
-								agv.selected=false
-								selectedAgv=null
-							}
-						}
-						if(agv2.destination.x==x&&agv2.destination.y==y)
-						{
-							flag=false
-						}
-
-					})
+					if(isOccupied)
+					{
+						flag=false
+					}
 					if(flag){
 						agv.destination.x=Math.floor(x)
 						agv.destination.y=Math.floor(y)
@@ -382,6 +356,7 @@ addEventListener('click', (event) =>
 	})
 
 
+var isOccupied=false
 
 
 canvas.onmousemove = function(event) {
@@ -393,23 +368,48 @@ canvas.onmousemove = function(event) {
 
         var x = xIndex * tileWidth,
          	y = yIndex * tileHeight,
-         	isOccupied=false,
          	isSelected=false,
-         	isSuper=true,
+         	isSuper=false,
          	selX=null,
          	selY=null;
+         	isOccupied=false;
+         	var selected_sagv=null;
+        agvs.forEach((agv) => {
+        	if(agv.super&&agv.selected){
+        		selected_sagv=agv
+        		isSuper=true
+        		if(((agv.y==agv.y2)&&((x+agv.tileWidth)>canvas.width+1))||((agv.x==agv.x2)&&((y+agv.tileHeight)>canvas.height+1)))
+        		{
+        			console.log("Here"+(x+agv.tileWidth),canvas.width)
+        			isOccupied=true
+        		}
+        	}
+        	if(agv.selected||agv.hover){
+	        	info.innerHTML = "AGV "+agv.id+"<br>X:"+Math.round(agv.x/tileWidth)+"<br>Y:"+Math.round(agv.y/tileHeight);
+				info.style.left = rect.left+agv.x+tileWidth+'px';
+				info.style.top = rect.top+agv.y+tileHeight+'px';
+				if(agv.selected){
+					agv.color = "#73D393"
+				}
+        	}
+
+        })
         agvs.forEach((agv) => {
         	if(agv.super){
 					agv.hover=(x==agv.x2&&y==agv.y2)
-					if((x==agv.x2&&y==agv.y2)||(x==agv.destination.x2&&y==agv.destination.y2)){
+					if(((x==agv.x2&&y==agv.y2)||(x==agv.destination.x2&&y==agv.destination.y2))&&(!isSuper)){
 						isOccupied=true
 					}
 				}
-			if(selectedAgv&&selectedAgv.super){
-				if((x+tileWidth==selectedAgv.x&&y==agv.y)||(x==agv.destination.x&&y==agv.destination.y)){
-				isOccupied=true
-			}
-
+			if(isSuper&&selected_sagv!=null){
+				if(!agv.selected){
+					if((((agv.x-x)==tileWidth)&&y==agv.y)&&(selected_sagv.y==selected_sagv.y2)){
+						isOccupied=true
+					}
+					else if((((agv.y-y)==tileHeight)&&x==agv.x)&&(selected_sagv.x==selected_sagv.x2)){
+						isOccupied=true
+					}
+				}
 			}
 			agv.hover=(x==agv.x&&y==agv.y)
 			if((x==agv.x&&y==agv.y)||(x==agv.destination.x&&y==agv.destination.y)){
@@ -439,11 +439,15 @@ canvas.onmousemove = function(event) {
 }
 
 var info = document.getElementById("info")
+info.style.width=tileWidth
 var menuNode = document.getElementById('menu');
+menuNode.style.width=tileWidth
 document.getElementById('enable').addEventListener('click', () => {
+	console.log("Enter")
 	var state=document.getElementById("enable").innerHTML
+	var r = canvas.getBoundingClientRect()
 	agvs.forEach((agv) => {
-		if((agv.x==(parseInt(menuNode.style.left)-tileWidth))&&(agv.y==(parseInt(menuNode.style.top) - tileHeight)))
+		if((agv.x==(parseInt(menuNode.style.left)-r.left-tileWidth))&&(agv.y==(parseInt(menuNode.style.top)-r.top - tileHeight)))
 		{
 			if(state=='Enable')
 			{
@@ -459,10 +463,12 @@ document.getElementById('enable').addEventListener('click', () => {
 document.getElementById('dock').addEventListener('click', () => {
 	var flag=true
 	var mode=document.getElementById('dock').innerHTML
+	var r = canvas.getBoundingClientRect()
 	if(mode=='Dock')
 	{
 		agvs.forEach((agv) => {
-			if((agv.x==(parseInt(menuNode.style.left)-tileWidth))&&(agv.y==(parseInt(menuNode.style.top) - tileHeight)))
+			console.log((agv.y,(parseInt(menuNode.style.top) - r.top - agv.tileHeight)))
+			if((agv.x==(parseInt(menuNode.style.left)-r.left-tileWidth))&&(agv.y==(parseInt(menuNode.style.top) - r.top - tileHeight)))
 			{
 				if(flag){
 					var agv2=checkNeighbors(1,agv)
@@ -478,7 +484,7 @@ document.getElementById('dock').addEventListener('click', () => {
 	}
 	else {
 		agvs.forEach((agv) => {
-			if((agv.x==(parseInt(menuNode.style.left)-tileWidth))&&(agv.y==(parseInt(menuNode.style.top) - tileHeight)))
+			if((agv.x==(parseInt(menuNode.style.left)-r.left-tileWidth))&&(agv.y==(parseInt(menuNode.style.top) - r.top - tileHeight)))
 			{
 				if(agv.super){
 					var agv1 = destroyAgv(agv)
@@ -547,8 +553,8 @@ canvas.addEventListener('contextmenu', function(event) {
 		document.getElementById("dock").innerHTML = "Undock"
 	}
 	menuNode.style.display = 'initial';
-	menuNode.style.left = x+tileWidth+'px';
-	menuNode.style.top = y+tileHeight+'px';
+	menuNode.style.left = rect.left+x+tileWidth+'px';
+	menuNode.style.top = rect.top+y+tileHeight+'px';
 })
 
 animate()
